01105ed0f2760b12a0108fd0f5c8a29f55a77300
ipsum <virgile@pop-os.localdomain>
2022-02-21 13:43:41 +0100

    remplacement de la bibliothèque music par gensound

--- convert.py
+++ convert.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import gensound
 
 def hilbert_approx(size=2, start=0, quarter='n'):
     if size == 2:
@@ -19,17 +21,18 @@ def hilbert_approx(size=2, start=0, quarter='n'):
         return mat + start
 
 from matplotlib.image import imread
-def load_img(path):
+def load_img(path, d=None):
     fractal = imread(path)
+    if d and d%2==0:
+        fractal = fractal[::d, ::d]
     image_map = np.zeros((len(fractal)**2, 4), dtype=int)
     image_map[:, 0] = hilbert_approx(len(fractal)).flatten()
     image_map[:, 1] = fractal[:, :, 0].ravel()
     image_map[:, 2] = fractal[:, :, 1].ravel()
     image_map[:, 3] = fractal[:, :, 2].ravel()
     image_map[np.argsort(image_map[:, 0])]
-    return image_map
+    return image_map, fractal
 
-import pandas as pd
 def load_notes():
     return pd.read_csv('notes.csv', delimiter='\t', names=['note', 'freq'])
 
@@ -46,33 +49,37 @@ def get_freq(df, pixel, taux_min=0, taux_max=100):
     val_max = taux_max * taille / 100
     return df['freq'][int(pixel * (val_max-val_min - 1) / 255 + val_min)]
 
+import argparse
+
 def main():
-    img = load_img('foret.jpeg')
+    parser = argparse.ArgumentParser(description='Transform input image to simple music.')
+    parser.add_argument(
+    '--input', '-i', type=str, default='foret.jpeg',
+        help="Input file (default: 'foret.jpeg').")
+    parser.add_argument(
+        '-n', type=int, default=1,
+        help="matrix dim")
+    args = parser.parse_args()
+    image_cible = args.input
+
+    img, img_raw = load_img(image_cible, args.n)
+    print(len(img_raw))
+    plt.ion()
+    plt.figure()
+    #imgplot = plt.imshow(img_raw[::args.n, ::args.n])
+    imgplot = plt.imshow(img_raw)
+    #plt.show()
+    plt.pause(0.001)
+
     notes = load_notes()
-    import music as M
-    T = M.tables.Basic()
-    H = M.utils.H
 
 
-    # 1) start a ѕynth
-    b = M.core.Being()
-
-    freq_seq = [get_freq(notes, img[i, 1], taux_min=40, taux_max=60) for i in range(len(img))]
+    #freq_seq = [get_freq(notes, img[i, 1], taux_min=40, taux_max=60) for i in range(len(img))]
+    notes_seq = [get_note(notes, img[i, 1], taux_min=40, taux_max=60) for i in range(len(img))]
     # 2) set its parameters using sequences to be iterated through
-    print(freq_seq[7000:9000])
-    #b.fv_ = [0, 1,5,15,150,1500,15000]  # vibrato frequency
-    b.fv_ = [0]  # vibrato frequency
-    b.nu_ = [0]  # vibrato depth in semitones (maximum deviation of pitch)
-    b.f_ = freq_seq[7000:9000]
-    b.d_ = [1 / 4] * len(b.f_)
+    wave = gensound.Sine(' '.join(notes_seq[:100]), duration=0.25e3)
+    print(notes_seq)
+    wave.play()
 
-    # 3) render the wavfile
-    b.render(120, 'aMusicalSound.wav')  #
-    # seq = []
-    # for i in range(len(img)):
-    #     seq.append(get_note(notes, img[i, 1]))
-    # gammes = [note[-1] for note in seq]
-    # print(gammes)
-    # print(len(seq), seq[:300])
 if __name__ == '__main__':
     main()
